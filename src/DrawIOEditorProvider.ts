135c6953f1277dcdc04962fb4fb258599a9f6ec2
eightHundreds <mingoing@outlook.com>
2020-04-10 20:43:57 +0800

    chore: update vscode 1.44

--- src/DrawIOEditorProvider.ts
+++ src/DrawIOEditorProvider.ts
@@ -1,45 +1,20 @@
 import * as vscode from "vscode";
-interface Edit {
-  readonly points: [number, number][];
-  readonly data: Uint8Array;
-}
 
-const viewType = "CustomDocument";
-export class DrawIOEditorProvider
-  implements vscode.CustomEditorProvider<Edit>, vscode.CustomEditorEditingDelegate<Edit> {
-  private _onDidEdit = new vscode.EventEmitter<vscode.CustomDocumentEditEvent<Edit>>();
-  onDidEdit: vscode.Event<vscode.CustomDocumentEditEvent<Edit>> = this._onDidEdit.event;
-  get editingDelegate() {
-    return this;
-  }
-  save(document: vscode.CustomDocument<Edit>, cancellation: vscode.CancellationToken): Thenable<void> {
-    return Promise.resolve();
-  }
-  saveAs(document: vscode.CustomDocument<Edit>, targetResource: vscode.Uri): Thenable<void> {
-    return Promise.resolve();
-  }
-  applyEdits(document: vscode.CustomDocument<Edit>, edits: readonly Edit[]): Thenable<void> {
-    return Promise.resolve();
-  }
-  undoEdits(document: vscode.CustomDocument<Edit>, edits: readonly Edit[]): Thenable<void> {
-    return Promise.resolve();
-  }
-  revert(document: vscode.CustomDocument<Edit>, edits: vscode.CustomDocumentRevert<Edit>): Thenable<void> {
-    return Promise.resolve();
-  }
-  backup(document: vscode.CustomDocument<Edit>, cancellation: vscode.CancellationToken): Thenable<void> {
-    return Promise.resolve();
+const viewType = "vscode-drawio.editor";
+export class DrawIOEditorProvider implements vscode.CustomTextEditorProvider{
+  static register(context: vscode.ExtensionContext){
+    const provider = new DrawIOEditorProvider(context);
+		const providerRegistration = vscode.window.registerCustomEditorProvider(viewType, provider);
+		return providerRegistration;
   }
 
-  openCustomDocument(uri: vscode.Uri, token: vscode.CancellationToken): Thenable<vscode.CustomDocument<Edit>> {
-    return Promise.resolve(new DrawIODocument(viewType,uri));
+  constructor(
+		private readonly context: vscode.ExtensionContext
+	) {
+
   }
-  resolveCustomEditor(
-    document: vscode.CustomDocument<Edit>,
-    webviewPanel: vscode.WebviewPanel,
-    token: vscode.CancellationToken
-  ): Thenable<void> {
-    return Promise.resolve();
+
+  resolveCustomTextEditor(document: vscode.TextDocument, webviewPanel: vscode.WebviewPanel, token: vscode.CancellationToken): void | Thenable<void> {
+    throw new Error("Method not implemented.");
   }
 }
-export class DrawIODocument extends vscode.CustomDocument<Edit> {}
